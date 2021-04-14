d7ddc35ceea09015bfd7f86e042d3bc1dd702d0b
Chunyi Lyu <clyu@pivotal.io>
2021-04-14 12:33:53 +0100

    Error msgs shouldn't be capitalized

--- controllers/common.go
+++ controllers/common.go
@@ -2,9 +2,9 @@ package controllers
 
 // common error messages shared across controllers
 const (
-	failedStatusUpdate         = "Failed to update object status"
-	failedMarshalSpec          = "Failed to marshal spec"
-	failedGenerateRabbitClient = "Failed to generate http rabbitClient"
+	failedStatusUpdate         = "failed to update object status"
+	failedMarshalSpec          = "failed to marshal spec"
+	failedGenerateRabbitClient = "failed to generate http rabbitClient"
 	noSuchRabbitDeletion       = "RabbitmqCluster is already gone: cannot find its connection secret"
 )
 
